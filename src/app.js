20ac6dd8f09f739c01f8e468452f14cf7b1cfaa4
UmedaMirai <umdmira128@gmail.com>
2022-06-08 21:52:21 +0900

    add: 2つの静的サイトを追加

--- src/app.js
+++ src/app.js
@@ -18,3 +18,15 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+// ------------------------------
+
+const mySite = express()
+mySite.use(express.static('src/my-site'))
+mySite.listen(3001, () => { console.log('[3001] OK') })
+
+const otherSite = express()
+otherSite.use(express.static('src/other-site'))
+otherSite.listen(3002, () => { console.log('[3002] OK') })
+
+
