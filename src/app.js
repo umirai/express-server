8f91f78680bfd6bd0cf0ed82868d2e137cdae9ed
UmedaMirai <umdmira128@gmail.com>
2022-06-09 01:05:02 +0900

    refactor: mySiteとotherSiteのファイルを分割

--- src/app.js
+++ src/app.js
@@ -5,6 +5,20 @@ const ngrok = require('ngrok')
 // ＊ ngrokアカウントを作成し、Your Authtokenを設定することで実際の動作を確認できます
 const TOKEN = ''
 
+const mySite = require('./mySite')
+const otherSite = require('./otherSite')
+mySite.listen(3001, () => { console.log('[3001] OK') })
+otherSite.listen(3002, () => { console.log('[3002] OK') })
+
+!(async function () {
+  await ngrok.authtoken(TOKEN)
+  const url = await ngrok.connect(3002)
+  console.log(`[${url}] OK`)
+
+  // mySiteにurlを渡す
+  mySite.locals.resourceURL = url
+})()
+
 // ------------------------------
 
 const app = express()
@@ -12,7 +26,7 @@ const port = 3000
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.status(200).json({ text: 'hello world' })
 })
 
@@ -26,54 +40,3 @@ app.post('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-// ------------------------------
-
-const mySite = express()
-mySite.use(cookieParser())
-mySite.set('view engine', 'ejs')
-mySite.get('/', (req, res) => {
-  res
-    .cookie('1stP cookie', 1, { httpOnly: true })
-    .render('index.ejs')
-})
-mySite.listen(3001, () => { console.log('[3001] OK') })
-
-// ------------------------------
-
-const otherSite = express()
-otherSite.use(express.static('other-site', {
-  setHeaders: (res) => {
-    res
-      .cookie('3rdP', 3, {
-        httpOnly: true,
-      })
-      .cookie('3rdP-Strict', 3, {
-        httpOnly: true,
-        sameSite: 'strict',
-      })
-      .cookie('3rdP-Lax', 3, {
-        httpOnly: true,
-        sameSite: 'lax',
-      })
-      .cookie('3rdP-None', 3, {
-        httpOnly: true,
-        sameSite: 'none',
-      })
-      .cookie('3rdP-None-Secure', 3, {
-        httpOnly: true,
-        sameSite: 'none',
-        secure: true,
-      })
-  }
-}))
-otherSite.listen(3002, () => { console.log('[3002] OK') })
-
-!(async function () {
-  await ngrok.authtoken(TOKEN)
-  const url = await ngrok.connect(3002)
-  console.log(`[${url}] OK`)
-
-  // mySiteにurlを渡す
-  mySite.locals.resourceURL = url
-})()
